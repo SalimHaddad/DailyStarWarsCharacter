33b2104596348eb0569c134dc168f52b9ef0a0a2
Salim <salim.elhaddad@lau.edu>
2018-03-18 16:29:39 +0200

    - recycler view done (app is still crashing) version 3 coming soon

--- app/src/main/java/com/salimdustries/dailystarwarscharacter/MainActivity.java
+++ app/src/main/java/com/salimdustries/dailystarwarscharacter/MainActivity.java
@@ -6,6 +6,10 @@ import com.android.volley.RequestQueue;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,6 +26,11 @@ import java.io.IOException;
 import java.util.List;
 import com.google.gson.Gson;
 
+import com.salimdustries.dailystarwarscharacter.api.character.CharacterApiManager;
+import com.salimdustries.dailystarwarscharacter.api.character.FilmApiManager;
+import com.salimdustries.dailystarwarscharacter.models.Adapter;
+import com.salimdustries.dailystarwarscharacter.models.Films;
+import com.salimdustries.dailystarwarscharacter.models.HeroInfo;
 import com.squareup.picasso.Picasso;
 
 import org.json.JSONException;
@@ -35,13 +44,15 @@ public class MainActivity extends AppCompatActivity {
 
 
     private Gson gson = new Gson();
-
+    CharacterApiManager  characterApiManager = new CharacterApiManager();
+    FilmApiManager fam = new FilmApiManager();
     private TextView hero ;
     private TextView year ;
     private ImageView hero_pic;
     Calendar cal = Calendar.getInstance();
     private int day = cal.get(Calendar.DAY_OF_MONTH);
-
+    private RecyclerView recyclerView ;
+    private Adapter mAdapter ;
    // private RecyclerView recyclerView ;
     //  private MyAdapter adapter ;
   //  private RecyclerView.LayoutManager layoutManager ;
@@ -51,62 +62,114 @@ public class MainActivity extends AppCompatActivity {
     private String sthero ;
     private String stplanet ;
     private String path = "R.drawable.img13" ;
+    private List<HeroInfo> list ;
+    private List<Films> films ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        rq = Volley.newRequestQueue(this);
+        recyclerView = (RecyclerView) findViewById(R.id.films_list);
+        mAdapter = new Adapter(films);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
+
+for (int i=1; i<8;i++) //episode 8 isn't added in the api
+{
+
+    fam.getFilmAtEpisode(i).enqueue(new retrofit2.Callback<Films>() {
+
+        @Override
+        public void onResponse(retrofit2.Call<Films> call, retrofit2.Response<Films> response) {
+            if(response.isSuccessful()){
+                Films film = response.body();
+                films.add(film);
+
+
+            }
+        }
+
+        @Override
+        public void onFailure(retrofit2.Call<Films> call, Throwable t) {
+
+        }
+
+
+    });
+
+
+
+}
+        characterApiManager.getHeroAtDay(day).enqueue(new retrofit2.Callback<HeroInfo>() {
+            @Override
+            public void onResponse(retrofit2.Call<HeroInfo> call, retrofit2.Response<HeroInfo> response) {
+                if(response.isSuccessful()){
+                    HeroInfo heroInfo = response.body();
+                    hero.setText(heroInfo.getName());
+                    year.setText(heroInfo.getBirthYear());
+
+                }
+            }
+
+            @Override
+            public void onFailure(retrofit2.Call<HeroInfo> call, Throwable t) {
+
+            }
+        });
+//
+//        rq = Volley.newRequestQueue(this);
         hero = (TextView) findViewById(R.id.hero_name);
-
-
-
+//
+//
+//
         year =(TextView) findViewById(R.id.birth_year);
         hero_pic = (ImageView)findViewById(R.id.hero_icon);
-        Picasso.with(this).load("R.drawable.img"+day).into(hero_pic);
-        int id = getResources().getIdentifier("img"+day,"drawable",getPackageName());
-        if(id != 0){
-            hero_pic.setImageResource(id);
+//        Picasso.with(this).load("R.drawable.img"+day).into(hero_pic);
+//        int id = getResources().getIdentifier("img"+day,"drawable",getPackageName());
+//        if(id != 0){
+//            hero_pic.setImageResource(id);
 //         Picasso.with(this).load(id);
-        }
-
-        Toasty.info(this, "Happy "+sthero+" day!", Toast.LENGTH_LONG, true).show();
-
-        if ( sthero != null) {
-            Log.d("evazan", sthero + " " + stplanet + " " + day);
-        }
-        sendHeroJsonRequest();
-
-    }
-
-    public void sendHeroJsonRequest(){
-        JsonObjectRequest jor = new JsonObjectRequest( com.android.volley.Request.Method.GET, url, null, new com.android.volley.Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response) {
-                try {
-                    sthero = response.getString("name");
-                    stplanet = response.getString("birth_year");
-
-                    hero.setText(sthero);
-                    year.setText(stplanet);
-                }
-
-                catch (JSONException e)
-                {
-                    e.printStackTrace();
-                }
-
-            }
-        }, new com.android.volley.Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-
-
-            }
-        }) ;
-
-
-        rq.add(jor);
+//        }
+//
+//        Toasty.info(this, "Happy "+sthero+" day!", Toast.LENGTH_LONG, true).show();
+//
+//        if ( sthero != null) {
+//            Log.d("evazan", sthero + " " + stplanet + " " + day);
+//        }
+//        sendHeroJsonRequest();
+//
+//    }
+//
+//    public void sendHeroJsonRequest(){
+//        JsonObjectRequest jor = new JsonObjectRequest( com.android.volley.Request.Method.GET, url, null, new com.android.volley.Response.Listener<JSONObject>() {
+//            @Override
+//            public void onResponse(JSONObject response) {
+//                try {
+//                    sthero = response.getString("name");
+//                    stplanet = response.getString("birth_year");
+//
+//                    hero.setText(sthero);
+//                    year.setText(stplanet);
+//                }
+//
+//                catch (JSONException e)
+//                {
+//                    e.printStackTrace();
+//                }
+//
+//            }
+//        }, new com.android.volley.Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//
+//
+//            }
+//        }) ;
+//
+//
+//        rq.add(jor);
     }
 }
